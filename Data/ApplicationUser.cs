fa1f6d8b108c285c19d2b77b36e7cbacea91238f
mourad.aissani <mourad.aissani@redfabriq.com>
2023-11-25 16:39:35 +0100

    Add project files.

---
+++ Data/ApplicationUser.cs
@@ -0,0 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Identity;
+
+namespace BlazingBlogV2.Data
+{
+    // Add profile data for application users by adding properties to the ApplicationUser class
+    public class ApplicationUser : IdentityUser
+    {
+        [Required,MaxLength(25)]
+        public string Name { get; set; }
+    }
+
+}
